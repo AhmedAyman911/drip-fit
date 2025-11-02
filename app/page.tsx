e387b5e4b9498e6422d34526d2e195a43c326baa
Ahmed Ayman <ahmedayman9113@gmail.com>
2025-11-02 17:44:35 +0200

    edits on the item card add sale and make inout dynamic throw props

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,48 @@ export default function Home() {
     <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
       <Navebar />
       <div className="pt-28 px-32 flex flex-wrap gap-10 justify-center">
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+        <ItemCard
+          isOnSale={true}
+          price={59.99}
+          salePrice={39.99}
+          title="Cool Sneakers"
+          imgSrc="/test.jpg"
+          category="Shoes"
+        />
+        <ItemCard
+          isOnSale={false}
+          price={89.99}
+          title="Leather Jacket"
+          imgSrc="/test.jpg"
+          category="Clothing"
+        />
+
+        <ItemCard
+          isOnSale={true}
+          price={49.99}
+          salePrice={44.99}
+          title="Graphic Tee"
+          imgSrc="/test.jpg"
+          category="T-Shirts"
+        />
+
+        <ItemCard
+          isOnSale={true}
+          price={19.99}
+          salePrice={14.99}
+          title="Classic Cap"
+          imgSrc="/test.jpg"
+          category="Accessories"
+        />
+
+        <ItemCard
+          isOnSale={true}
+          price={59.99}
+          salePrice={39.99}
+          title="Cool Sneakers"
+          imgSrc="/test.jpg"
+          category="Shoes"
+        />
       </div>
     </div>
   );
--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,23 +1,49 @@
 import Link from "next/link"
 import Image from "next/image"
 
+interface ItemCardProps {
+    title: string;
+    imgSrc: string;
+    price: number;
+    category: string;
+    isOnSale: boolean;
+    salePrice?: number
+}
 
-export default function ItemCard() {
+
+export default function ItemCard({ isOnSale, title, imgSrc, price, category, salePrice }: ItemCardProps) {
     return (
         <Link href={'/item'} className="flex flex-col w-80">
-            <Image alt="item img"
-                src={'/test.jpg'}
-                width={512}
-                height={512}
-                className="object-cover"
-            />
+            <div className="relative">
+                <Image alt="item img"
+                    src={imgSrc}
+                    width={512}
+                    height={512}
+                    className="object-cover"
+                />
+                {isOnSale && (
+                    <span className="absolute top-3 right-3 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded-2xl">
+                        Sale
+                    </span>
+                )}
+            </div>
             <div className="p-3">
-                <p className="text-sm text-gray-400 dark:text-gray-500">category</p>
-                <p className="text-lg md:text-2xl font-bold">title</p>
-                <div className="flex">
-                    <span className="text-md font-bold">123</span>
-                    <span className="text-md font-bold text-green-400">&nbsp;$</span>
-                </div>
+                <p className="text-sm text-gray-400 dark:text-gray-500">{category}</p>
+                <p className="text-lg md:text-2xl font-bold">{title}</p>
+                {isOnSale ?
+                    <div className="flex">
+                        <span className="text-md font-bold line-through">{price}</span>
+                        <div className="flex px-2">
+                            <span className="text-md font-bold text-red-400">{salePrice}</span>
+                            <span className="text-md font-bold text-green-400">&nbsp;$</span>
+                        </div>
+                    </div>
+                    :
+                    <div className="flex">
+                        <span className="text-md font-bold">{price}</span>
+                        <span className="text-md font-bold text-green-400">&nbsp;$</span>
+                    </div>
+                }
             </div>
         </Link>
     )
