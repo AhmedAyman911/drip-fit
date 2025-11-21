1baf836008b65d1c21738e10125a88ed96002d8e
Ahmed Ayman <ahmedayman9113@gmail.com>
2025-11-21 17:05:00 +0200

    Add skeleton loading for new arrivals carousel in the landing page

--- components/Landing page/NewArrivals.tsx
+++ components/Landing page/NewArrivals.tsx
@@ -1,9 +1,16 @@
 'use client'
 import CoustumeCarousel from "./CoustumeCarousel"
 import { useNewArrivals } from '@/hooks/useNewArrivals'
+import LandingCarouselSkeleton from "../skeletons/LandingCarousel"
+
 
 export default function NewArrivals() {
-    const { data: newArrivals, error } = useNewArrivals()
+    const { data: newArrivals, isLoading, error } = useNewArrivals()
+
+    if (isLoading) {
+        return <LandingCarouselSkeleton />
+    }
+
     if (error) return <p>Error fetching sale products</p>
     return (
         <CoustumeCarousel title="New Arrivals" products={newArrivals ?? []} />
---
+++ components/skeletons/LandingCarousel.tsx
@@ -0,0 +1,23 @@
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import { ItemCardSkeleton } from "./ItemCard";
+
+export default function LandingCarouselSkeleton() {
+    return (
+        <section className="pt-12 bg-gray-100 dark:bg-gray-900">
+            <div className="container mx-auto max-w-6xl">
+                <Carousel>
+                    <h2 className="md:text-left text-center md:text-3xl text-2xl font-bold mb-8 md:px-5">
+                        New Arrivals
+                    </h2>
+                    <CarouselContent className="px-6">
+                        {Array.from({ length: 6 }).map((_, index) => (
+                            <CarouselItem key={index} className="basis-8xl">
+                                <ItemCardSkeleton />
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                </Carousel>
+            </div>
+        </section>
+    )
+}
